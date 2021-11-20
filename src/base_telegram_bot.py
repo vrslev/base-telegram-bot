f98fdb85e01a0248432e03f3a8f375ce2108595d
Lev <mail@vrslev.com>
2021-11-20 18:36:14 +0300

    Initial commit

---
+++ src/base_telegram_bot.py
@@ -0,0 +1,62 @@
+from json import JSONDecodeError
+from typing import Any, Literal, TypeVar
+
+import requests
+from pydantic import BaseModel
+
+__all__ = ["BaseTelegramBot"]
+
+
+class TelegramBotError(Exception):
+    def __init__(self, *args: Any, response: requests.Response) -> None:
+        self.response = response
+        super().__init__(*args)
+
+
+class BaseTelegramBotResponse(BaseModel):
+    ok: Literal[True]
+    result: Any
+
+
+_T = TypeVar("_T")
+
+
+class BaseTelegramBot:
+    endpoint = "https://api.telegram.org"
+
+    def __init__(self, token: str):
+        self.token = token
+        self._session = requests.Session()
+
+    def _build_url(self, method: str):
+        assert method.startswith("/"), "Method name should start with slash"
+        return f"{self.endpoint}/bot{self.token}{method}"
+
+    def parse_response(
+        self, *, response: requests.Response, model: type[_T] = BaseTelegramBotResponse
+    ) -> _T:
+        try:
+            resp_json = response.json()
+        except JSONDecodeError:
+            raise TelegramBotError("Can't decode json response", response=response)
+
+        if "description" in resp_json:
+            response.reason = resp_json["description"]
+
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as exc:
+            raise TelegramBotError(*exc.args, response=response)
+
+        return model(**resp_json)
+
+    def make_request(
+        self,
+        *,
+        method: str,
+        model: type[_T] = BaseTelegramBotResponse,
+        json: Any = None,
+    ) -> _T:
+        url = self._build_url(method)
+        response = self._session.post(url=url, json=json)
+        return self.parse_response(response=response, model=model)
---
+++ test_base_telegram_bot.py
@@ -0,0 +1,156 @@
+from copy import copy
+from json import JSONDecodeError
+from typing import Any
+
+import pytest
+import requests
+import responses
+from pydantic import BaseModel
+
+from base_telegram_bot import BaseTelegramBot, BaseTelegramBotResponse, TelegramBotError
+
+
+@pytest.fixture
+def token():
+    return "some token value"
+
+
+@pytest.fixture
+def bot(token: str):
+    return BaseTelegramBot(token)
+
+
+def test_build_url_not_slash(bot: BaseTelegramBot):
+    with pytest.raises(AssertionError, match="Method name should start with slash"):
+        bot._build_url("getUpdates")
+
+
+def test_build_url_default_endpoint(bot: BaseTelegramBot):
+    default_endpoint = copy(bot.endpoint)
+    assert bot._build_url("/getUpdates").startswith(default_endpoint)
+
+
+def test_build_url_custom_endpoint(bot: BaseTelegramBot):
+    custom_endpoint = "https://example.com"
+    bot.endpoint = copy(custom_endpoint)
+    assert bot._build_url("/getUpdates").startswith(custom_endpoint)
+
+
+def test_build_url_main(bot: BaseTelegramBot, token: str):
+    method = "/getUpdates"
+    assert bot._build_url(copy(method)) == f"{bot.endpoint}/bot{token}{method}"
+
+
+def test_parse_response_not_json(bot: BaseTelegramBot):
+    class MyResponse:
+        def json(self):
+            raise JSONDecodeError("", "", 0)
+
+    response = MyResponse()
+    with pytest.raises(TelegramBotError, match="Can't decode json response") as exc:
+        bot.parse_response(response=response)  # type: ignore
+    assert exc.value.response == response
+
+
+def test_parse_response_no_description(bot: BaseTelegramBot):
+    class MyResponse:
+        reason: str | None = None
+
+        def json(self) -> dict[Any, Any]:
+            return {}
+
+        def raise_for_status(self):
+            raise requests.HTTPError
+
+    response = MyResponse()
+    with pytest.raises(TelegramBotError):
+        bot.parse_response(response=response)  # type: ignore
+    assert response.reason is None
+
+
+def test_parse_response_with_description(bot: BaseTelegramBot):
+    class MyResponse:
+        reason: str | None = None
+
+        def json(self) -> dict[Any, Any]:
+            return {"description": "Not Found"}
+
+        def raise_for_status(self):
+            raise requests.HTTPError
+
+    response = MyResponse()
+    with pytest.raises(TelegramBotError):
+        bot.parse_response(response=response)  # type: ignore
+    assert response.reason == "Not Found"
+
+
+def test_parse_response_http_error(bot: BaseTelegramBot):
+    args = ("one", "two")
+
+    class MyResponse:
+        def json(self) -> dict[Any, Any]:
+            return {}
+
+        def raise_for_status(self):
+            raise requests.HTTPError(*copy(args))
+
+    response = MyResponse()
+    with pytest.raises(TelegramBotError) as exc:
+        bot.parse_response(response=response)  # type: ignore
+    assert exc.value.args == args
+    assert exc.value.response == response
+
+
+def test_parse_response_returns_default_model(bot: BaseTelegramBot):
+    exp_result = "my result"
+
+    class MyResponse:
+        def json(self) -> dict[Any, Any]:
+            return {"ok": True, "result": exp_result}
+
+        def raise_for_status(self):
+            pass
+
+    assert bot.parse_response(
+        response=MyResponse()  # type: ignore
+    ) == BaseTelegramBotResponse(ok=True, result=exp_result)
+
+
+def test_parse_response_returns_custom_model(bot: BaseTelegramBot):
+    exp_result = {"foo": "foo", "bar": 1}
+
+    class CustomResponseResult(BaseModel):
+        foo: str
+        bar: int
+
+    class CustomResponse(BaseTelegramBotResponse):
+        result: CustomResponseResult
+
+    class MyResponse:
+        def json(self) -> dict[Any, Any]:
+            return {"ok": True, "result": exp_result}
+
+        def raise_for_status(self):
+            pass
+
+    assert bot.parse_response(
+        response=MyResponse(), model=CustomResponse  # type: ignore
+    ) == CustomResponse(ok=True, result=exp_result)
+
+
+@responses.activate
+def test_make_request(bot: BaseTelegramBot, token: str):
+    responses.add(
+        method=responses.POST,
+        url=f"https://api.telegram.org/bot{token}/getUpdates",
+        json={"ok": True, "result": {"foo": "bar"}},
+        match=[responses.matchers.json_params_matcher({"id": 101})],  # type: ignore
+    )
+
+    class GetUpdatesResult(BaseModel):
+        foo: str
+
+    class GetUpdatesResponse(BaseTelegramBotResponse):
+        result: GetUpdatesResult
+
+    bot.make_request(method="/getUpdates", model=GetUpdatesResponse, json={"id": 101})
